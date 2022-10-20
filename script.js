d413702f918a8eb8416038add032105d8bd85282
pochkacraft <112497320+pochkacraft@users.noreply.github.com>
2022-10-20 12:43:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function ModalPlasmovoice(){
 }
 
 function ModalHistory(){
-	ModalContent.innerHTML = '<h1>Прошлые сезоны</h1><div class = "flexbox history"><h2>1 Сезон</h2> <span class = "info-history">25.09.2021 - 1.16.5</span> <a href = "https://drive.google.com/uc?export=download&id=1fvt-3xF-XH2H3ajxGu25K30eygWN7Xr2"><button>Скачать карту</button></a></div> <div class = "flexbox history"><h2>2 Сезон</h2> <span class = "info-history">01.12.2021 - 1.16.5</span><a href="https://drive.google.com/uc?export=download&id=11-rmihQbqtSqgmj3nDDxV2fMpB25ZnZb"><button>Скачать карту</button></a></div> <div class = "instruct-div"><a href="https://drive.google.com/file/d/1KbXZDlluLVhnoQXkBYDBI9e_FRnSAI_E/view?usp=sharing" class = "instruct">Инструкция</a></div>';
+	ModalContent.innerHTML = '<h1>Прошлые сезоны</h1><div class = "flexbox history"><h2>1 Сезон</h2> <span class = "info-history">25.09.2021 - 1.16.5</span> <a href = "https://drive.google.com/uc?export=download&id=1fvt-3xF-XH2H3ajxGu25K30eygWN7Xr2"><button>Скачать карту</button></a></div> <div class = "flexbox history"><h2>2 Сезон</h2> <span class = "info-history">01.12.2021 - 1.16.5</span><a href="https://drive.google.com/uc?export=download&id=11-rmihQbqtSqgmj3nDDxV2fMpB25ZnZb"><button>Скачать карту</button></a></div> <div class = "flexbox history"><h2>3 Сезон</h2> <span class = "info-history">23.09.2022 - 1.19.2</span><a href="https://drive.google.com/drive/folders/1b9NMykZaKtob2nSXNE8gDo7Rrbgo52ZP?usp=sharing"><button>Скачать карту</button></a></div> <div class = "instruct-div"><a href="https://drive.google.com/file/d/1KbXZDlluLVhnoQXkBYDBI9e_FRnSAI_E/view?usp=sharing" class = "instruct">Инструкция</a></div>';
 }
 
 function ModalPremium(){
